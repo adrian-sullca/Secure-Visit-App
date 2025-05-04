b25652771905baef4fb21c7296014bf317d4c4bc
adrian-sullca <adrian.infooo@gmail.com>
2025-05-04 20:46:31 +0200

    Add enum VisitType

--- backend/app/ActionType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App;
-
-enum ActionType:string
-{
-    case ENTRY = 'entry';
-    case EXIT = 'exit';
-}
---
+++ backend/app/VisitType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App;
+
+enum VisitType: string
+{
+    case FAMILY = 'family';
+    case PROFESSIONAL = 'professional';
+}
